61a3eb7ee4412bf52f39521c9fc012b277335527
Maja Ferle <maja.ferle@in516ht.com>
2025-08-02 20:36:45 +0200

    setup scripts

---
+++ setup_git_integration.sql
@@ -0,0 +1,12 @@
+-- ------------------------------------
+-- Set up GIT integration for all users
+-- ------------------------------------
+USE ROLE ACCOUNTADMIN;
+CREATE OR REPLACE API INTEGRATION WS_DEMO_GIT_INTEGRATION
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/mferle')
+  API_USER_AUTHENTICATION = (TYPE = SNOWFLAKE_GITHUB_APP)
+  ENABLED = TRUE;
+
+-- grant usage on the API integration to all users
+GRANT USAGE ON INTEGRATION WS_DEMO_GIT_INTEGRATION TO ROLE PUBLIC;
